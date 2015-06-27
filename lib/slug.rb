28f5eb04db5242231c78f093a649bf6bfcbf7acf
HackFisher <hackfisher@gmail.com>
2015-06-27 14:27:18 +0800

    Fix slug generation issue

--- lib/slug.rb
+++ lib/slug.rb
@@ -20,9 +20,18 @@ module Slug
   private
 
   def self.ascii_generator(string)
-    string.gsub("'", "")
-          .parameterize
-          .gsub("_", "-")
+    # TODO review if ja should use this
+    # ko asked for it to be removed
+    if ['zh_CN', 'ja'].include?(SiteSetting.default_locale)
+      unless defined? Stringex
+        require 'stringex_lite'
+      end
+      slug = string.to_url
+    else
+      slug = string.gsub("'", "").parameterize
+      slug.gsub!("_", "-")
+    end    +
+    slug =~ /[^\d]/ ? slug : '' # Reject slugs that only contain numbers, because they would be indistinguishable from id's.
   end
 
   def self.encoded_generator(string)
